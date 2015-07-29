68e8e4215231f96d4ffdfb5cdbffb4d9f21ace79
Pranabesh <pranabesh.sinha@gmail.com>
2015-07-29 00:39:25 -0700

    Recursive version of append . shorter form

--- src/nlp/RegEx.scala
+++ src/nlp/RegEx.scala
@@ -6,29 +6,29 @@ package nlp
 import scala.collection.immutable.Set
 import scala.collection.mutable.Stack
 
-class PostFix(){
+class PostFix() {
   val rpn = new Stack[Char]()
   val operator = new Stack[Char]()
   val operators: Set[Char] = Set('+', '*', '-', '(', ')', '/', '^')
   val unaryOperator: Set[Char] = Set('+', '*')
-  val precedence = Map( '^' -> 4,
-                        '*' -> 3,
-                        '/' -> 3,
-                        '+' -> 2,
-                        '-' -> 2,
-                        '(' -> 0,
-                        ')' -> 0)
+  val precedence = Map('^' -> 4,
+    '*' -> 3,
+    '/' -> 3,
+    '+' -> 2,
+    '-' -> 2,
+    '(' -> 0,
+    ')' -> 0)
 
   def in2post(input: String): String = {
-    for(ch <- input) {
-      if(ch.isLetterOrDigit || ch == '.') rpn.push(ch)
-      else if(operators.contains(ch) && !unaryOperator.contains(ch)) {
+    for (ch <- input) {
+      if (ch.isLetterOrDigit || ch == '.') rpn.push(ch)
+      else if (operators.contains(ch) && !unaryOperator.contains(ch)) {
         handleOperators(ch)
       }
       else
         rpn.push(ch)
     }
-    while(!operator.isEmpty)
+    while (!operator.isEmpty)
       rpn.push(operator.pop())
     rpn.mkString("").reverse
   }
@@ -37,82 +37,47 @@ class PostFix(){
     input match {
       case '(' => operator.push(input)
       case ')' => {
-        while(!operator.isEmpty && operator.top != '(')
+        while (!operator.isEmpty && operator.top != '(')
           rpn.push(operator.pop())
         operator.pop()
       }
-      case _ =>  {
-        while(!operator.isEmpty && precedence(operator.top) >= precedence(input))
+      case _ => {
+        while (!operator.isEmpty && precedence(operator.top) >= precedence(input))
           rpn.push(operator.pop())
         operator.push(input)
       }
     }
   }
 
-  def appendConcatRecursive(input: String, result: String): String = {
-    var tempResult = result
-    println(s"input = $input")
-    println(s"input result $result")
+  def appendConcat(input: String, result: String): String = {
+    if(input.length == 0) return result
+    var tempResult = result + input.head
     //terminating condition
-    if(input.length == 0) tempResult
-    else if(input.length == 1){
-      tempResult += input + "."
-    }
-    else if(input(0) == '('){
-      tempResult +=  "(" + input(1)
-      tempResult = appendConcatRecursive(input.slice(2, input.indexOf(')')), tempResult) + ")"
-      tempResult = appendConcatRecursive(input.slice(input.indexOf(')')+1, input.length), tempResult)
-    }
-    else if(unaryOperator.contains(input(0))){
-      tempResult += input(0) + "."
-      tempResult = appendConcatRecursive(input.slice(1, input.length), tempResult)
-    }
-    else if(!operators.contains(input(0)) && !unaryOperator.contains(input(1))){
-      tempResult += input(0) + "."
-      tempResult = appendConcatRecursive(input.slice(1, input.length), tempResult)
-    }
-    else if(unaryOperator.contains(input(1))){
-      tempResult += input(0)
-      tempResult = appendConcatRecursive(input.slice(1, input.length), tempResult)
-    }
-    println(s"result $tempResult")
-    tempResult
-  }
+    if (input.length == 1) return tempResult + "."
 
-  def appendConcatOperator(input: String): String = {
-    var result: String = input.head.toString
-    var i = 1
-    println(s"input $input")
-    val endConcat = if(result(0).isLetterOrDigit) true else false
-    while(i < input.length-1){
-      if(unaryOperator.contains(input(i)))
-        result += input(i) + "."
-      else if(!operators.contains(input(i)) && !unaryOperator.contains(input(i + 1)))
-        result += input(i) + "."
-      else if(unaryOperator.contains(input(i + 1)))
-        result += input(i)
-      else if(input(i) == '('){
-        result += input(i) + appendConcatOperator(input.slice(i+1, input.indexOf(')'))) + ')'
-        i = input.indexOf(')')
-      }
-      i += 1
+    if (input(0) == '(') {
+      tempResult += input(1)
+      tempResult = appendConcat(input.slice(2, input.indexOf(')')), tempResult) + ")"
+      tempResult = appendConcat(input.slice(input.indexOf(')') + 1, input.length), tempResult)
     }
-    if(input(input.length-1) != ')')
-      result += input(input.length-1)
-    if(endConcat) result += "."
-    println(s"result $result")
-    result
+    else{
+      if(!unaryOperator.contains(input(1))){
+        tempResult += "."
+      }
+      tempResult = appendConcat(input.slice(1, input.length), tempResult)
+    }
+    tempResult
   }
 }
 
 object PostFix{
   def main(args: Array[String]) {
     val obj = new PostFix()
-    val input = "a(bb)+a(xxx)*"
+    val input = "ab(dd)*(de)+"
     var str = input.head.toString
-    str = obj.appendConcatRecursive(input.tail, str)
+    str = obj.appendConcat(input.tail, str)
     println(str)
-//    println(obj.in2post(str))
+    println(obj.in2post(str))
   }
 
 }
