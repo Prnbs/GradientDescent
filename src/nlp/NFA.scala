d517d3d32c6c57f5827ca3433f51af0798e5ac82
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2015-07-29 16:42:36 -0700

    Added NFA,mostly copied

--- src/nlp/NFA.scala
+++ src/nlp/NFA.scala
@@ -18,6 +18,16 @@ case class Repeat(expr: RegexExpr) extends RegexExpr
 // a+
 case class Plus(expr: RegexExpr) extends RegexExpr
 
+abstract class State
+
+class Consume(val c: Char, val out: State) extends State
+
+class Split(val out1: State, val out2: State) extends State
+
+case class Match() extends State
+
+class Placeholder(var pointingTo: State) extends State
+
 object NFA {
 
   def postFix2AST(postFixInput: String): RegexExpr = {
@@ -39,14 +49,33 @@ object NFA {
     AST.pop()
   }
 
+  def ast2Nfa(ast: RegexExpr, andThen: State): State = {
+    ast match {
+      case Literal(c) => new Consume(c, andThen)
+      case Concat(left, right) =>
+                         ast2Nfa(left, ast2Nfa(right, andThen))
+      case Repeat(c) => {
+                          val placeholder = new Placeholder(null)
+                          val split = new Split(
+                                      ast2Nfa(c, placeholder),
+                                      andThen
+                          )
+                          placeholder.pointingTo = split
+                          placeholder
+                        }
+      case Plus(c) =>  ast2Nfa(Concat(c, Repeat(c)), andThen)
+    }
+  }
+
   def main(args: Array[String]) {
     val obj = new PostFix()
-    val input = "ab(dd)*"
+    val input = "ab*"
     var str = input.head.toString
     str = obj.appendConcat(input.tail, str)
     val postFix = obj.in2post(str)
     println(s"Postfixed $postFix")
     var expr = postFix2AST(postFix)
+    var state = ast2Nfa(expr, Match())
     println("Done")
 
   }
