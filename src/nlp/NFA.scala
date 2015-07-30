76bb0f4ee3fb17e234ee4f9502a282597168cf58
Pranabesh Sinha <pranabesh.sinha@gmail.com>
2015-07-29 17:49:36 -0700

    Added NFA evaluation

--- src/nlp/NFA.scala
+++ src/nlp/NFA.scala
@@ -20,13 +20,13 @@ case class Plus(expr: RegexExpr) extends RegexExpr
 
 abstract class State
 
-class Consume(val c: Char, val out: State) extends State
+case class Consume(val c: Char, val out: State) extends State
 
-class Split(val out1: State, val out2: State) extends State
+case class Split(val out1: State, val out2: State) extends State
 
 case class Match() extends State
 
-class Placeholder(var pointingTo: State) extends State
+case class Placeholder(var pointingTo: State) extends State
 
 object NFA {
 
@@ -53,30 +53,51 @@ object NFA {
     ast match {
       case Literal(c) => new Consume(c, andThen)
       case Concat(left, right) =>
-                         ast2Nfa(left, ast2Nfa(right, andThen))
+        ast2Nfa(left, ast2Nfa(right, andThen))
       case Repeat(c) => {
-                          val placeholder = new Placeholder(null)
-                          val split = new Split(
-                                      ast2Nfa(c, placeholder),
-                                      andThen
-                          )
-                          placeholder.pointingTo = split
-                          placeholder
-                        }
-      case Plus(c) =>  ast2Nfa(Concat(c, Repeat(c)), andThen)
+        val placeholder = new Placeholder(null)
+        val split = new Split(
+          ast2Nfa(c, placeholder),
+          andThen
+        )
+        placeholder.pointingTo = split
+        placeholder
+      }
+      case Plus(c) => ast2Nfa(Concat(c, Repeat(c)), andThen)
     }
   }
 
+    def evaluateNFA(currState: State, stringToMatch: String): Boolean = {
+      currState match {
+        case Consume(c, andThen) => {
+          if(stringToMatch.isEmpty) return true
+          if(stringToMatch.head != c) return false
+          evaluateNFA(andThen, stringToMatch.tail)
+        }
+        case Placeholder(pointingTo) => {
+          evaluateNFA(pointingTo, stringToMatch)
+        }
+        case Split(out1, out2) => {
+          evaluateNFA(out1, stringToMatch)
+          evaluateNFA(out2, stringToMatch)
+        }
+        case Match() => {
+          return true
+        }
+      }
+    }
+
   def main(args: Array[String]) {
     val obj = new PostFix()
-    val input = "ab*"
+    val input = "a(b)*"
     var str = input.head.toString
     str = obj.appendConcat(input.tail, str)
     val postFix = obj.in2post(str)
     println(s"Postfixed $postFix")
-    var expr = postFix2AST(postFix)
-    var state = ast2Nfa(expr, Match())
-    println("Done")
+    val expr = postFix2AST(postFix)
+    val state = ast2Nfa(expr, Match())
+    val result: Boolean = evaluateNFA(state, "aaaaaabbbbbbbbbbbbbbbbbbb")
+    println(result)
 
   }
 }
